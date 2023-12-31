ac6b9a396eb7017cce388f6b61d44f2bf0d9fb58
Bruno <bseilliebert@outlook.fr>
2023-12-31 16:18:39 +0100

    store analyze in db, little cleaning and front updated for history db

---
+++ src/main/frontend/src/components/ResolvingTagsModal.tsx
@@ -0,0 +1,22 @@
+function ResolvingTagsModal() {
+    return (
+        <div class="modal fade" id="modal" data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="modalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="modalLabel">Resolving Tags</h1>
+                    </div>
+                    <div class="modal-body">
+                        <div class="d-flex justify-content-center">
+                            <div class="spinner-border" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ResolvingTagsModal
